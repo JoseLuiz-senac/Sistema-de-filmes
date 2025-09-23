af6895e2e8bcd06c05e07abafa646b9d83c6447d
Bia <ana.lima4568556@edu.pe.senac.br>
2025-09-23 08:51:33 -0300

    .

--- src/main/java/com/sistemadefilmes/entity/FilmesEntidade.java
+++ src/main/java/com/sistemadefilmes/entity/FilmesEntidade.java
@@ -1,5 +1,77 @@
 package com.sistemadefilmes.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class FilmesEntidade {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String genero;
+    private int anoLancamento;
+    private String diretor;
+    private double nota; 
+
+
+    public FilmesEntidade(String nome, String genero, int anoLancamento, String diretor, double nota) {
+        this.nome = nome;
+        this.genero = genero;
+        this.anoLancamento = anoLancamento;
+        this.diretor = diretor;
+        this.nota = nota;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
 }
