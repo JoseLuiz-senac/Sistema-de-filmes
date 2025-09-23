b404092100f8c93282e1e53fe93969bfe55b227c
Jose <jose.tavares4928884@edu.pe.senac.br>
2025-09-23 08:55:00 -0300

    Terminando o service

--- src/main/java/com/sistemadefilmes/service/FilmesService.java
+++ src/main/java/com/sistemadefilmes/service/FilmesService.java
@@ -10,12 +10,44 @@ import org.springframework.stereotype.Service;
 public class FilmesService {
     private final FilmesRepository filmesRepository;
 
+    //Metodo construtor
     public FilmesService(FilmesRepository repo) {
         this.filmesRepository = repo;
     }
 
+    //Metodo de listar todos
     public list<Filmes> ListarTodos() {
         return filmesRepository.findAll();
     }
+
+    //Metodo de listar por id
+    public Optional<Filmes> ListarPorId(Long id) {
+        return filmesRepository.findById(id);
+    }
+
+    //Metodo de listar por nome
+    public list<Filmes> ListarporNome(String nome) {
+        return filmesRepository.findByNome(nome);
+    }
     
+
+    //Metodo de adicionar filme
+    public Filmes AdicionarFilme(Filmes filme) {
+        return filmesRepository.save(filme);
+    }
+
+    //Metodo de atualizar filme
+    public Filmes AtualizarFilme(Filmes filme) {
+
+        if (filme.getnome().isBlank()){
+            throw new IllegalArgumentException("O nome do filme não encontrado ou não pode ser vazio.");
+        }
+        return filmesRepository.save(filme);
+    }
+
+    //Metodo de deletar filme
+    public boolean ExcluirFilme(long id) {
+        return filmesRepository.deleteById(id);
+    }
+
 }
