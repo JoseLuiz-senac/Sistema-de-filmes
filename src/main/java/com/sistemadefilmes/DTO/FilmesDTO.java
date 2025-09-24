32b18af131bee689a629438cb0a24cf84c701b98
CAIO AUGUSTUS ARAÚJO JORGE <caio.jorge4955520@edu.pe.senac.br>
2025-09-24 08:40:42 -0300

    olha a porra do codigo josé seu merda

--- src/main/java/com/sistemadefilmes/DTO/FilmesDTO.java
+++ src/main/java/com/sistemadefilmes/DTO/FilmesDTO.java
@@ -1,5 +1,61 @@
 package com.sistemadefilmes.DTO;
 
 public class FilmesDTO {
+    private String nome;
+    private String genero;
+    private String diretor;
+    private int anoLancamento;
+    private double nota;
+
+    public FilmesDTO(String nome, String genero, int anoLancamento, String diretor, double nota) {
+        this.nome = nome;
+        this.genero = genero;
+        this.anoLancamento = anoLancamento;
+        this.diretor = diretor;
+        this.nota = nota;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+
+
+
     
 }
